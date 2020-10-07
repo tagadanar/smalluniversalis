66d5fdbcda97c7efee7fcc7d8bc09b4fed6fb437
RENON Raphael <raphael.renon@vinci-construction.com>
2020-10-07 17:34:24 +0200

    first draft of a mainmenu

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public bool isStart;
+    public bool isMap;
+    public bool isQuit;
+    void OnMouseUp(){
+	if(isStart) {
+		// Application.LoadLevel(1);
+	}
+
+    if(isMap) {
+		GetComponent<Renderer>().material.color=Color.cyan;
+    }
+    
+	if (isQuit) {
+		Application.Quit();
+	}
+} 
+}
