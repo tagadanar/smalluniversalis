73e482c16d09db6446a2f3125af97b1fcc948b26
RENON Raphael <raphael.renon@vinci-construction.com>
2020-10-14 16:56:29 +0200

    reorganizing

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -9,11 +10,13 @@ public class MainMenu : MonoBehaviour
     public bool isQuit;
     void OnMouseUp(){
 	if(isStart) {
-		// Application.LoadLevel(1);
+		// todo
 	}
 
     if(isMap) {
-		GetComponent<Renderer>().material.color=Color.cyan;
+		// Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        Debug.Log("Load:mapedit");
+		SceneManager.LoadScene("mapedit", LoadSceneMode.Single);
     }
     
 	if (isQuit) {
